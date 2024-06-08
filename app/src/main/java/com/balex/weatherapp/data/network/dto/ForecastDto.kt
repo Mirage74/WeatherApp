81b179f16ad5fd6e368d74322a2a412cfa38b653
balex <balexvicx@gmail.com>
2024-06-09 01:00:01 +0200

    Dto

---
+++ app/src/main/java/com/balex/weatherapp/data/network/dto/ForecastDto.kt
@@ -0,0 +1,7 @@
+package com.balex.weatherapp.data.network.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class ForecastDto(
+    @SerializedName("forecastday") val forecastDay: List<DayDto>
+)
