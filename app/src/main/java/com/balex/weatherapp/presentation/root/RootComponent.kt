60402cb025ba930a08ec12752fd436bace9c66d5
balex <balexvicx@gmail.com>
2024-06-13 20:41:00 +0200

    RootComponent 1

--- app/src/main/java/com/balex/weatherapp/presentation/root/RootComponent.kt
+++ app/src/main/java/com/balex/weatherapp/presentation/root/RootComponent.kt
@@ -1,4 +1,21 @@
 package com.balex.weatherapp.presentation.root
 
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.balex.weatherapp.presentation.details.DetailsComponent
+import com.balex.weatherapp.presentation.favourite.FavouriteComponent
+import com.balex.weatherapp.presentation.search.SearchComponent
+
 interface RootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+
+    sealed interface Child {
+
+        data class Favourite(val component: FavouriteComponent) : Child
+
+        data class Search(val component: SearchComponent) : Child
+
+        data class Details(val component: DetailsComponent) : Child
+    }
 }
