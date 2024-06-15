5a5cb7ba846faaf6a59769accee32502fb36abad
balex <balexvicx@gmail.com>
2024-06-15 19:47:46 +0200

    search content realized

--- app/src/main/java/com/balex/weatherapp/data/network/api/ApiFactory.kt
+++ app/src/main/java/com/balex/weatherapp/data/network/api/ApiFactory.kt
@@ -6,10 +6,12 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.util.Locale
 
 object ApiFactory {
 
     private const val KEY_PARAM = "key"
+    private const val PARAM_LANG = "lang"
     private const val BASE_URL = "https://api.weatherapi.com/v1/"
 
 
@@ -29,6 +31,7 @@ object ApiFactory {
                 .url
                 .newBuilder()
                 .addQueryParameter(KEY_PARAM, BuildConfig.WEATHER_API_KEY)
+                .addQueryParameter(PARAM_LANG, Locale.getDefault().language)
                 .build()
             val newRequest = originalRequest.newBuilder()
                 .url(newUrl)
